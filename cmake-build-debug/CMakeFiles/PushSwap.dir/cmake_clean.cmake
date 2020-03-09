ab12a7e94179d36b6f5e4395f2a02065e0c5d09b
signedcash <sap-i@bk.ru>
2020-03-09 23:55:39 +0300

    done

---
+++ cmake-build-debug/CMakeFiles/PushSwap.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PushSwap.dir/main.c.obj"
+  "CMakeFiles/PushSwap.dir/srcs/swap.c.obj"
+  "PushSwap.exe"
+  "PushSwap.exe.manifest"
+  "PushSwap.pdb"
+  "libPushSwap.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PushSwap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
